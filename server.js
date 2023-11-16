f4e747cd3565df49ccd73e925dd7679c7a3a94f4
Zoi <zoilanbond@gmail.com>
2023-11-15 16:26:34 -0800

    get 1 and post route created

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ const pool = new Pool({
 app.use((express.static("public")))
 app.use(express.json());
 
+//hash password middleware
+app.use(async (req, res, next) => {
+    if (req.body.password) {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        req.body.password = hashedPassword;
+    }
+    next();
+})
 
+//get all route
 app.get('/api/:endpoint', async (req, res, next) => {
     try {
         const {endpoint} = req.params;
@@ -23,7 +32,7 @@ app.get('/api/:endpoint', async (req, res, next) => {
         let query;
         switch (endpoint) {
             case 'users':
-                query = `Select username FROM ${endpoint}`;
+                query = `Select id, username FROM ${endpoint}`;
                 break;
             case 'tasks':
                 query = `SELECT * FROM ${endpoint}`;
@@ -39,7 +48,71 @@ app.get('/api/:endpoint', async (req, res, next) => {
     }
 })
 
+//get 1
+app.get('/api/:endpoint/:id', async (req, res, next) =>{
+    try {
+        const {id, endpoint} = req.params;
+        if (isNaN(id)) {
+            errorMessage()
+        }
+        let query;
+        let values;
+        switch (endpoint) {
+            case 'users':
+                query = `SELECT users.id, users.username, tasks.*
+                FROM users
+                LEFT JOIN tasks ON users.id = tasks.user_id
+                WHERE users.id = $1`;
+                values = [id]
+                break;
+            case 'tasks':
+                query = `SELECT tasks.*, users.id as user_id, users.username
+                FROM tasks
+                LEFT JOIN users ON tasks.user_id = users.id
+                WHERE tasks.id = $1`;
+                values = [id]
+                break;
+            default:
+                res.status(404).send('NOT FOUND 🙃');
+                return;
+        }
+        const result = await pool.query(query, values);
+        if (result.rows.length === 0) {
+            errorMessage()
+        }
+        res.status(200).send(result.rows);
+    } catch(err) {
+        next(err)
+    }
+})
 
+//post route 
+app.post('/api/:endpoint', async (req, res, next) => {
+    try {
+        const { endpoint } = req.params;
+        const { username, password, email, title, description, user_id } = req.body;
+
+        let query;
+        let values;
+        switch(endpoint) {
+            case 'users':
+                query=`INSERT INTO users (username, email, password) VALUES($1, $2, $3) RETURNING id, username`;
+                values=[username, email, password]
+                break;
+            case 'tasks':
+                query=`INSERT INTO tasks (title, description, user_id) VALUES ($1, $2, $3) RETURNING *`;
+                values=[title, description, user_id]
+                break;
+            default:
+                res.status(404).send('NOT FOUND 🙃');
+                return;
+        }
+        const result = await pool.query(query, values);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        next(err)
+    }
+})
 
 
 
@@ -56,3 +129,9 @@ app.use((err, req, res, next) => {
 app.listen(PORT, (req, res) => {
     console.log(`Listening in on port: ${PORT}`)
 })
+
+function errorMessage() {
+    const err = new Error('NOT FOUND 🙃');
+    err.status = 404;
+    throw err;
+}
